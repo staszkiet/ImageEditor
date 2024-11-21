c64197b5e94632b6ac1c10fe212aa5f7aff0bc62
Stanisław <62651497+staszkiet@users.noreply.github.com>
2024-11-21 16:31:38 +0100

    Added structures with inheritance (not used)

--- ComputerGraphics3/ModificationManager.cs
+++ ComputerGraphics3/ModificationManager.cs
@@ -19,4 +19,31 @@ namespace ComputerGraphics3
             return Color.FromArgb(255 - r.R, 255 - r.G, 255 - r.B);
         }
     }
+
+    public class BrightnessManager : ModificationManager
+    {
+        public static int light = 100;
+        public override Color ModifyPixel(Color r)
+        {
+            return Color.FromArgb(Helpers.Brighten(r.R, light), Helpers.Brighten(r.G, light), Helpers.Brighten(r.B, light));
+        }
+    }
+
+    public class ContrastManager : ModificationManager
+    {
+        public static int contrast_low = 55;
+        public static int constrast_high = 200;
+        public override Color ModifyPixel(Color c)
+        {
+            return Color.FromArgb(Helpers.Contrast(c.R, contrast_low, constrast_high), Helpers.Contrast(c.G, contrast_low, constrast_high), Helpers.Contrast(c.B, contrast_low, constrast_high));
+        }
+    }
+    public class GammaManager : ModificationManager
+    {
+        public static double gamma = 2d;
+        public override Color ModifyPixel(Color r)
+        {
+            return Color.FromArgb(Helpers.nigamma(r.R, gamma), Helpers.nigamma(r.G, gamma), Helpers.nigamma(r.B, gamma));
+        }
+    }
 }
